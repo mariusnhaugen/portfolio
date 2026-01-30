272ad72ed6c69a889b22ab9240786666d4780dba
Marius <mariusnhaugen@gmail.com>
2026-01-30 10:40:34 +0000

    fix: content collection dir

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,15 @@
 import {defineCollection, z} from 'astro:content';
-
-//loaders
 import {glob,file} from 'astro/loaders';
 
 
 const projects = defineCollection({
-    loader: glob({pattern: "**/*.md", base: "./src/pages"}),
+    loader: glob({pattern: "**/*.md", base: "./src/content/projects"}),
     schema: z.object({
         title: z.string(),
         description: z.string(),
         slug: z.string(),
         sortOrder: z.number().optional(),
     }) 
-
 })
-
 
 export const collections = { projects }
